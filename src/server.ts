0611ede71916cb782bdf5bce362c59c9b4983849
Sammuel Reis <sammugr@hotmail.com>
2022-03-06 17:16:38 -0300

    chore: implements errors handler

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
+import "express-async-errors";
 import "dotenv/config";
 
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 
 import CONFIG from "./config/env";
 import { routes } from "./routes";
@@ -11,6 +12,18 @@ app.set("port", CONFIG.port);
 app.use(express.json());
 
 app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    message: "Internal server error",
+  });
+});
 
 app.listen(3001, () =>
   console.log(`Server is running on PORT: ${CONFIG.port} `)
